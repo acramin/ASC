c3307a1b5284eb0180bd3f933f6b2f123906e64f
acramin <132026903+acramin@users.noreply.github.com>
2025-02-22 09:14:45 -0300

    exemplos básicos

---
+++ aula01/index.js
@@ -0,0 +1,21 @@
+// Declarando constantes
+const nome = "José";
+const idade = 27;
+console.log(nome, idade);
+
+//idade = 21; // dá problema pois a constante não pode ser alterada
+// aspas simples e duplas dão na mesma
+
+const genero = "M";
+const endereco = "Rua Olho D'Água, 23";
+console.log(nome, idade, genero, endereco);
+
+// declarando variável locais
+let a = 2;
+let b = "abc";
+console.log(a, b);
+
+// declarando variáveis globais - recomendável não usar
+var c = 2 + 3;
+var d = "defg";
+console.log(c, d);
