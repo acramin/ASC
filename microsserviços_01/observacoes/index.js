8db9fc284b8dd2b5fb7d31a283be45531b499468
acramin <132026903+acramin@users.noreply.github.com>
2025-04-26 10:39:48 -0300

    finalização microsserviço de observacoes

--- microsserviços_01/observacoes/index.js
+++ microsserviços_01/observacoes/index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const { v4: uuidv4 } = require("uuid");
+
+const observacoesPorLembreteID = {};
 
 app.get("/lembretes/:id/observacoes", (req, res) => {
-
-
+  res.send(observacoesPorLembreteID[req.params.id] || []);
 });
 
 app.post("/lembretes/:id/observacoes", (req, res) => {
-
-    
+  //:id é um placeholder
+  const idObs = uuidv4();
+  const { texto } = req.body;
+  const observacoesDoLembrete = observacoesPorLembreteID[req.params.id] || []; // req.params pega os parâmetros da url
+  observacoesDoLembrete.push({ id: idObs, texto });
+  observacoesPorLembreteID[req.params.id] = observacoesDoLembrete;
+  res.status(201).send(observacoesDoLembrete);
 });
 
 app.listen(5000, () => console.log("Observações up. Port 4000"));
